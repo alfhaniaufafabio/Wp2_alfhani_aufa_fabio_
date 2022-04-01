63511bfafb6d6328436ab8c93a9f17df37595d07
alfhaniaufafabio <ifanaufa@gmail.com>
2022-04-01 17:49:44 +0700

    penambahan ada di readme

---
+++ pustaka-booking/application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?= $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : AlfhaniAufaFabio</li>
+        <li>Nama Belakang : alfani</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> jl.cideng barat no.93</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah raga Favorit </h4>
+    <ul type="square">
+        <li>Pelari</li>
+        <li>Berenang</li>
+    </ul>
+</section>
